2a0e50062a0a23f79102f07f5db251a98bc72d5f
LloydTao <LL23@live.co.uk>
2020-09-26 01:18:30 +0100

    ➕ Install Nuxt Storybook

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -47,4 +47,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  // Storybook configuration (https://storybook.nuxtjs.org/options)
+  storybook: {},
 }
